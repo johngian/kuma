846ba3e89cc2b619a129581b8678c155fca6f062
John Giannelos <jgiannelos@mozilla.com>
2019-07-17 17:35:08 +0200

    Extend fields for BC signal serializer

--- kuma/api/v1/serializers.py
+++ kuma/api/v1/serializers.py
@@ -4,16 +4,25 @@ from rest_framework import serializers
 from kuma.wiki.models import BCSignal, Document
 
 
-class BCSignalSerializer(serializers.Serializer):
+class BCSignalSerializer(serializers.ModelSerializer):
     slug = serializers.CharField()
     locale = serializers.CharField()
 
+    class Meta:
+        model = BCSignal
+        fields = ('browser', 'feature', 'comment', 'links', 'slug', 'locale')
+
     def create(self, validated_data):
+        slug = validated_data.pop('slug')
+        locale = validated_data.pop('locale')
+
         document = Document.objects.filter(
-            slug=validated_data['slug'],
-            locale=validated_data['locale']
+            slug=slug, locale=locale
         ).first()
 
         if document:
-            return BCSignal.objects.create(document=document)
+            return BCSignal.objects.create(
+                document=document,
+                **validated_data
+            )
         raise exceptions.ValidationError('Document not found')
